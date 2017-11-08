4320159be6abae6f5ed10446fbe21a0cdc05417b
programmiertier <zimmermannveronique@protonmail.com>
2017-11-08 09:18:15 +0100

    Anzeigen, Entladen
    Methoden aus Interface löschen

--- Interface_ConsoleApplication/IAnzeige.cs
+++ Interface_ConsoleApplication/IAnzeige.cs
@@ -7,15 +7,13 @@ using static System.Console;
 
 namespace Interface_ConsoleApplication
 {
-    public interface IAnzeige       // vorher ICaption
+    interface IAnzeige       // vorher ICaption
     {
         // nur die Methoden erklärt, nicht implementiert
 
-        void Anzeige(string s);        // der Vertrag, was implementierende Klassen umsetzen müssen
-                                       // Konventionsgemäß wird der implementierung von interface ein großes I voran gestellt
-                                       // Methode vom Interface auch groß, damit man weiß, dass es aus dem Interface stammt
-        
-        
+        void Anzeige(string s);         // der Vertrag, was implementierende Klassen umsetzen müssen
+                                        // Konventionsgemäß wird der implementierung von interface ein großes I voran gestellt
+                                        // Methode vom Interface auch groß, damit man weiß, dass es aus dem Interface stammt
+        void Entladen();                // wenn aus IAnzeige gelöscht, wird überall die Implementierung angemäkelt!
     }
-
 }
